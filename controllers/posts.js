c3c7780230a65c218ada3b122da4383bc50b3361
Simon Paul <samonpaul06@gmail.com>
2023-05-22 14:39:38 +0530

    updated post route

--- controllers/posts.js
+++ controllers/posts.js
@@ -19,7 +19,7 @@ export const createPost = async (req, res) => {
     });
     await newPost.save();
 
-    const post = await Post.find();
+    const post = await Post.find().sort({ createdAt: -1 });
     res.status(201).json(post);
   } catch (err) {
     res.status(409).json({ message: err.message });
@@ -30,7 +30,6 @@ export const createPost = async (req, res) => {
 export const getFeedPosts = async (req, res) => {
   try {
     const post = await Post.find().sort({ createdAt: -1 });
-    console.log(post);
     res.status(200).send(post);
   } catch (err) {
     res.status(404).send({ message: err.message });
